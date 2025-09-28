049820e73a46f1b70046431a4b9451e0789694a5
amrsamy <amrsamy622@gmail.com>
2025-09-28 18:26:09 +0300

    update cartRoutes.js and server.js

--- NodeJS/Day-4/backend/routes/cartRoutes.js
+++ NodeJS/Day-4/backend/routes/cartRoutes.js
@@ -2,3 +2,16 @@ const express = require("express");
 const router = express.Router();
 const authMiddleware = require("../middlewares/auth");
 
+const {
+  getCart,
+  addCart,
+  updateCartById,
+  deleteCartById,
+} = require("../controllers/cartController");
+
+router.get("/", authMiddleware, getCart);
+router.post("/", authMiddleware, addCart);
+router.put("/:id", authMiddleware, updateCartById);
+router.delete("/:id", authMiddleware, deleteCartById);
+
+module.exports = router;
--- NodeJS/Day-4/backend/server.js
+++ NodeJS/Day-4/backend/server.js
@@ -1,6 +1,7 @@
 const bookRoutes = require("./routes/bookRoutes");
 const userRoutes = require("./routes/userRoutes");
 const authRoutes = require("./routes/authRoutes");
+const cartRoutes = require("./routes/cartRoutes");
 const logger = require("./middlewares/logger");
 const express = require("express");
 const connectDB = require("./db/db");
@@ -35,6 +36,7 @@ app.use(logger);
 app.use(createApiPrefix("/auth"), authRoutes);
 app.use(createApiPrefix("/users"), userRoutes);
 app.use(createApiPrefix("/books"), bookRoutes);
+app.use(createApiPrefix("/carts"), cartRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${process.env.PORT}`);
